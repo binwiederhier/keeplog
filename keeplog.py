af1c95e1cf193790d3936f11f0d9b6f2ea572c69
Philipp Heckel <pheckel@datto.com>
2020-11-01 09:20:43 -0500

    derp

--- keeplog.py
+++ keeplog.py
@@ -1,6 +1,13 @@
 import gkeepapi
 import re
+from datetime import datetime
 from os.path import expanduser
+
+class Entry:
+    def __init__(self, title, text, date):
+        self.title = title
+        self.text = text
+        self.date = date
 
 def main():
     # read config
@@ -25,44 +32,61 @@ def main():
     # parse log
     log = {}
     with open(expanduser(file)) as f:
-        prevline = ""
-        title = ""
         for line in f:
-            if prevline != "":
-                if line.strip() == "--": # and re.search('^\d+/\d+/\d+ ', prevline):
-                    title = prevline.strip()
-                elif title != "" and prevline.strip() != "--":
-                    if title in log:
-                        log[title] = log[title] + prevline
-                    else:
-                        log[title] = prevline
-            prevline = line
-        if title in log:
-            log[title] = log[title] + prevline
-        else:
-            log[title] = prevline
+            match = re.search('^(\d+)/(\d+)/(\d+) ', line)
+            if match:
+                title = line.strip()
+                date = datetime(year=2000+int(match.group(3)), month=int(match.group(1)), day=int(match.group(2)))
+                log[title] = Entry(title, "", date)
+            elif line.strip() != "--" and title in log:
+                log[title].text = log[title].text + line
 
-    for k in log:
-        print(k)
-        print("--")
-        print(log[k], end="")
-    print(log)
-    exit(1)
+  #  for k in log:
+  #      print(log[k].title)
+  #      print("--")
+  #      print(log[k].text, end="")
+
+    #exit(1)
 
     # read keep
     # App password, see https://myaccount.google.com/apppasswords
     keep = gkeepapi.Keep()
     keep.login(username, password)
 
+    notes = {}
+    label = keep.findLabel('log')
+
     note: gkeepapi.node.TopLevelNode
-    for note in keep.all():
+    for note in keep.find(labels=[label]):
+        if not re.search('^\d+/\d+/\d+ ', note.title):
+            print(f"{note.title} - Skipping, title mismatch")
+            continue
+        notes[note.title] = note
 
-        label: gkeepapi.node.Label
-        for label in note.labels:
-            if label.name == "log":
-                print(label)
+   # for title in notes.keys():
+   #     note = notes[title]
+   #     print(note.title)
+   #     print("-- ")
+   #     print(note.text, end="")
 
-            print(note.title)
+    #exit(1)
+
+    for entry in log.values():
+        if not entry.title in notes:
+            print(f"{entry.title} - Creating remotely")
+            note = keep.createNote(entry.title, entry.text)
+            note.timestamps.created = entry.date
+            note.timestamps.edited = entry.date
+            note.labels.add(label)
+        elif notes[entry.title].text != entry.text:
+            print(f"{entry.title} - Updating remotely")
+            notes[title].text = entry.text
+            notes[title].timestamps.created = entry.date
+            notes[title].timestamps.edited = entry.date
+        else:
+            print(f"{entry.title} - Up to date")
+
+    keep.sync()
 
 if __name__ == '__main__':
     main()
