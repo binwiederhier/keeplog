e9dc8427e39bbade0996096a70640ae0094869d0
Philipp Heckel <pheckel@datto.com>
2020-11-08 12:31:32 -0500

    State file

--- keeplog.py
+++ keeplog.py
@@ -7,26 +7,24 @@ import os
 import re
 import shutil
 import sys
+import gkeepapi
 from datetime import datetime
 from os.path import expanduser, exists, join
-from pathlib import Path
-
-import gkeepapi
-
 
 class Keeplog:
     def __init__(self, logger, config):
         self.logger = logger
         self.config = config
+        self.state = State()
         self.keep = gkeepapi.Keep()
 
     def sync(self):
+        self.read_state()
         self.login()
-        self.write_token()
 
         local = self.read_local()
         remote = self.read_remote()
-        sync = self.read_sync()
+        checksums = self.state.checksums
 
         # synchronizing
         self.logger.info("Comparing remote and local")
@@ -34,51 +32,50 @@ class Keeplog:
         label = self.keep.getLabel(self.config.label)
 
         for title in local.keys():
-            if not title in remote:
+            if title not in remote:
                 self.logger.info(f"- Creating remotely: {title}")
                 note = self.keep.createNote(title, local[title].text())
                 note.labels.add(label)
-                sync[title] = {"checksum": local[title].checksum()}
+                checksums[title] = local[title].checksum()
             elif remote[title].text() != local[title].text():
-                if title in sync:
-                    local_changed = local[title].checksum() != sync[title]["checksum"]
-                    remote_changed = remote[title].checksum() != sync[title]["checksum"]
+                if title in checksums:
+                    local_changed = local[title].checksum() != checksums[title]
+                    remote_changed = remote[title].checksum() != checksums[title]
                     if local_changed and not remote_changed:
                         self.logger.info(f"- Updating remotely: {title}")
                         remote[title].text(local[title].text())
-                        sync[title] = {"checksum": local[title].checksum()}
+                        checksums[title] = local[title].checksum()
                     elif not local_changed and remote_changed:
                         self.logger.info(f"- Updating locally: {title}")
                         local[title].text(remote[title].text())
-                        sync[title] = {"checksum": remote[title].checksum()}
+                        checksums[title] = remote[title].checksum()
                         local_updated = True
                     elif self.config.on_conflict == "prefer-remote":
                         self.logger.info(f"- Updating locally (conflict override): {title}")
                         local[title].text(remote[title].text())
-                        sync[title] = {"checksum": remote[title].checksum()}
+                        checksums[title] = remote[title].checksum()
                         local_updated = True
                     elif self.config.on_conflict == "prefer-local":
                         self.logger.info(f"- Updating remotely (conflict override): {title}")
                         remote[title].text(local[title].text())
-                        sync[title] = {"checksum": local[title].checksum()}
+                        checksums[title] = local[title].checksum()
                     else:
                         self.logger.info(f"- Conflict, doing nothing: {title}")
                 elif self.config.on_conflict == "prefer-remote":
                     self.logger.info(f"- Updating locally (conflict override): {title}")
                     local[title].text(remote[title].text())
-                    sync[title] = {"checksum": remote[title].checksum()}
+                    checksums[title] = remote[title].checksum()
                     local_updated = True
                 elif self.config.on_conflict == "prefer-local":
                     self.logger.info(f"- Updating remotely (conflict override): {title}")
                     remote[title].text(local[title].text())
-                    sync[title] = {"checksum": local[title].checksum()}
+                    checksums[title] = local[title].checksum()
                 else:
                     self.logger.info(f"- Conflict, doing nothing: {title}")
             else:
-                sync[title] = {"checksum": local[title].checksum()}
+                checksums[title] = local[title].checksum()
 
         self.keep.sync()
-        self.write_state()
 
         if local_updated:
             self.backup_local()
@@ -86,7 +83,12 @@ class Keeplog:
         else:
             self.logger.info("Nothing to update locally")
 
-        self.write_sync(sync)
+        self.state.keep = self.keep.dump()
+        self.state.checksums = checksums
+        self.state.write(self.config.state_file)
+
+    def read_state(self):
+        self.state.load(self.config.state_file)
 
     def read_local(self):
         self.logger.info("Reading local notes")
@@ -120,32 +122,6 @@ class Keeplog:
 
         return remote
 
-    def read_token(self):
-        token = None
-        if exists(self.config.token_file):
-            with open(self.config.token_file) as f:
-                token = f.read().strip()
-        return token
-
-    def read_state(self):
-        state = None
-        if exists(self.config.state_file):
-            with open(self.config.state_file) as f:
-                state = json.load(f)
-        return state
-
-    def read_sync(self):
-        sync = {}
-        if exists(self.config.sync_file):
-            with open(self.config.sync_file, encoding='utf-8') as f:
-                data = json.load(f)
-                if not "notes" in data:
-                    raise ValueError(f"Invalid sync file format in file {self.config.sync_file}")
-                notes = data["notes"]
-                for title in notes.keys():
-                    sync[title] = notes[title]
-        return sync
-
     def backup_local(self):
         os.makedirs(self.config.backup_dir, exist_ok=True)
         backup_file = join(self.config.backup_dir, datetime.now().strftime("%y%m%d%H%M%S"))
@@ -154,24 +130,21 @@ class Keeplog:
     def login(self):
         logged_in = False
 
-        token = self.read_token()
-        state = self.read_state()
-
-        if token:
-            logged_in = self.login_with_token(token, state)
+        if self.state.token:
+            logged_in = self.login_with_token()
 
         if not logged_in:
-            logged_in = self.login_with_password(state)
+            logged_in = self.login_with_password()
 
         if not logged_in:
             raise Exception("Failed to authenticate")
 
-    def login_with_token(self, token, state):
+    def login_with_token(self):
         self.logger.info('Logging in with token')
         logged_in = False
 
         try:
-            self.keep.resume(self.config.username, token, state=state, sync=True)
+            self.keep.resume(self.config.username, self.state.token, state=self.state.keep, sync=True)
             self.logger.info('Successfully logged in')
             logged_in = True
         except gkeepapi.exception.LoginException:
@@ -179,36 +152,19 @@ class Keeplog:
 
         return logged_in
 
-    def login_with_password(self, state):
+    def login_with_password(self):
         self.logger.info('Logging in with password')
         logged_in = False
 
         try:
-            self.keep.login(self.config.username, self.config.password, state=state, sync=True)
+            self.keep.login(self.config.username, self.config.password, state=self.state.keep, sync=True)
             self.logger.info('Successfully logged in')
+            self.state.token = self.keep.getMasterToken()
             logged_in = True
         except gkeepapi.exception.LoginException:
             self.logger.info('Login failed')
 
         return logged_in
-
-    def write_token(self):
-        self.make_parent(self.config.token_file)
-        token = self.keep.getMasterToken()
-        with open(self.config.token_file, 'w') as f:
-            f.write(token)
-
-    def write_state(self):
-        self.make_parent(self.config.state_file)
-        state = self.keep.dump()
-        with open(self.config.state_file, 'w') as f:
-            json.dump(state, f)
-
-    def write_sync(self, sync):
-        self.make_parent(self.config.sync_file)
-        with open(self.config.sync_file, mode="w", encoding="utf-8") as f:
-            data = {"notes": sync}
-            json.dump(data, f)
 
     def write_local(self, local):
         self.logger.info("Writing local notes")
@@ -219,10 +175,6 @@ class Keeplog:
                 f.write(local[title]["text"] + "\n")
                 if not local[title]["text"].endswith("\n"):
                     f.write("\n")  # ensure empty line between entries
-
-    def make_parent(self, file):
-        Path(file).parent.mkdir(mode=0o755, parents=True, exist_ok=True)
-
 
 class Note:
     def text(self, v=None):
@@ -252,23 +204,48 @@ class LocalNote(Note):
         return self.content
 
 
+class State:
+    def __init__(self):
+        self.token = None
+        self.keep = None
+        self.checksums = {}
+
+    def load(self, file):
+        if exists(file):
+            with open(file, encoding='utf-8') as f:
+                state = json.load(f)
+                if "token" in state:
+                    self.token = state["token"]
+                if "keep" in state:
+                    self.keep = state["keep"]
+                if "checksums" in state:
+                    self.checksums = state["checksums"]
+        return self
+
+    def write(self, file):
+        with open(file, mode="w", encoding="utf-8") as f:
+            data = {
+                "token": self.token,
+                "keep": self.keep,
+                "checksums": self.checksums
+            }
+            json.dump(data, f)
+
 class Config:
     def __init__(self):
         self.username = None
         self.password = None
         self.file = None
-        self.label = "log"
+        self.label = "keeplog"
         self.on_conflict = "prefer-local"
-        self.sync_file = expanduser("~/.keeplog/state/sync")
-        self.state_file = expanduser("~/.keeplog/state/state")
-        self.token_file = expanduser("~/.keeplog/state/token")
+        self.state_file = expanduser("~/.keeplog/state")
         self.backup_dir = expanduser("~/.keeplog/backups")
 
-    def load(self, config_file):
-        if not exists(config_file):
-            raise Exception("Config file " + config_file + " does not exist")
+    def load(self, file):
+        if not exists(file):
+            raise Exception("Config file " + file + " does not exist")
 
-        with open(config_file, encoding='utf-8') as cfg:
+        with open(file, encoding='utf-8') as cfg:
             for line in cfg:
                 match = re.search("^([^=]+)=(.+)", line)
                 if match:
@@ -280,12 +257,8 @@ class Config:
                         self.file = expanduser(match.group(2))
                     elif match.group(1) == "label":
                         self.label = match.group(2)
-                    elif match.group(1) == "sync-file":
-                        self.sync_file = expanduser(match.group(2))
                     elif match.group(1) == "state-file":
                         self.state_file = expanduser(match.group(2))
-                    elif match.group(1) == "token-file":
-                        self.token_file = expanduser(match.group(2))
                     elif match.group(1) == "on-conflict":
                         self.on_conflict = match.group(2)
                     elif match.group(1) == "backup-dir":
