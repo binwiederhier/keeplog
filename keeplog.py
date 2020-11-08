e70a3b3d3b79f0efdbd8f9d5eab1bad05a1b1c7c
Philipp Heckel <pheckel@datto.com>
2020-11-07 22:36:07 -0500

    Restructure

--- keeplog.py
+++ keeplog.py
@@ -9,6 +9,7 @@ import shutil
 import sys
 from datetime import datetime
 from os.path import expanduser, exists, join
+from pathlib import Path
 
 import gkeepapi
 
@@ -20,222 +21,23 @@ def main():
     keeplog = Keeplog(logger, config)
     keeplog.sync()
 
-    # parse log
-    logger.info("Parsing local log " + config.file)
-    local = read_local(config.file)
-    logger.info("Read " + str(len(local)) + " entries")
-
-    # read keep
-    keep = gkeepapi.Keep()
-    logged_in = False
-
-    token = read_token(config.token_file)
-    state = read_state(config.state_file)
-
-    if token:
-        logger.info('Authenticating with token')
-
-        try:
-            keep.resume(config.username, token, state=state, sync=True)
-            logged_in = True
-            del token
-            logger.info('Successfully logged in and synced state')
-        except gkeepapi.exception.LoginException:
-            logger.warning('Invalid token, falling back to password')
-
-    if not logged_in:
-        logger.info('Authenticating with password')
-
-        try:
-            keep.login(config.username, config.password, state=state, sync=True)
-            logged_in = True
-
-            token = keep.getMasterToken()
-            with open(config.token_file, 'w') as f:
-                f.write(token)
-
-            logger.info('Successfully logged in')
-        except gkeepapi.exception.LoginException:
-            logger.info('Login failed')
-
-    if not logged_in:
-        logger.error('Failed to authenticate')
-        sys.exit(1)
-
-    # Read notes
-    logger.info("Reading Keep notes")
-
-    remote = read_remote(keep, logger)
-
-    logger.info("Read " + str(len(remote)) + " notes from Keep")
-
-    # read sync file
-    sync = read_sync(config.sync_file)
-
-    # synchronizing
-    logger.info("Updating remote")
-    local_updated = False
-    label = keep.getLabel('log')
-
-    for title in local.keys():
-        if not title in remote:
-            logger.info(f"- Creating remotely: {title}")
-            note = keep.createNote(title, local[title]["text"])
-            note.labels.add(label)
-            sync[title] = {"checksum": md5(local[title]["text"])}
-        elif remote[title]["note"].text != local[title]["text"]:
-            if title in sync:
-                local_changed = local[title]["checksum"] != sync[title]["checksum"]
-                remote_changed = remote[title]["checksum"] != sync[title]["checksum"]
-                if local_changed and not remote_changed:
-                    logger.info(f"- Updating remotely: {title}")
-                    remote[title]["note"].text = local[title]["text"]
-                    sync[title] = {"checksum": md5(local[title]["text"])}
-                elif not local_changed and remote_changed:
-                    logger.info(f"- Updating locally: {title}")
-                    local[title]["text"] = remote[title]["note"].text
-                    sync[title] = {"checksum": md5(remote[title]["note"].text)}
-                    local_updated = True
-                elif config.on_conflict == "prefer-remote":
-                    logger.info(f"- Updating locally (conflict override): {title}")
-                    local[title]["text"] = remote[title]["note"].text
-                    sync[title] = {"checksum": md5(remote[title]["note"].text)}
-                    local_updated = True
-                elif config.on_conflict == "prefer-local":
-                    logger.info(f"- Updating remotely (conflict override): {title}")
-                    remote[title]["note"].text = local[title]["text"]
-                    sync[title] = {"checksum": md5(local[title]["text"])}
-                else:
-                    logger.info(f"- Conflict, doing nothing: {title}")
-            elif config.on_conflict == "prefer-remote":
-                logger.info(f"- Updating locally (conflict override): {title}")
-                local[title]["text"] = remote[title]["note"].text
-                sync[title] = {"checksum": md5(remote[title]["note"].text)}
-                local_updated = True
-            elif config.on_conflict == "prefer-local":
-                logger.info(f"- Updating remotely (conflict override): {title}")
-                remote[title]["note"].text = local[title]["text"]
-                sync[title] = {"checksum": md5(local[title]["text"])}
-            else:
-                logger.info(f"- Conflict, doing nothing: {title}")
-        else:
-            sync[title] = {"checksum": md5(local[title]["text"])}
-
-    keep.sync()
-    state = keep.dump()
-    with open(config.state_file, 'w') as f:
-        json.dump(state, f)
-    logger.info("Done")
-
-    if local_updated:
-        logger.info("Updating log file")
-        backup_local(config.backup_dir, config.file)
-
-        with open(config.file, mode="w", encoding="utf-8") as f:
-            for title in local.keys():
-                f.write(title + "\n")
-                f.write("--\n")
-                f.write(local[title]["text"] + "\n")
-                if not local[title]["text"].endswith("\n"):
-                    f.write("\n")  # ensure empty line between entries
-    else:
-        logger.info("Nothing to update locally")
-
-    # write sync file
-    logger.info("Writing sync file")
-    with open(config.sync_file, mode="w", encoding="utf-8") as f:
-        data = {"notes": sync}
-        json.dump(data, f)
-
-
-def read_local(local_file):
-    local = {}
-
-    # parse file
-    with open(local_file, encoding='utf-8') as f:
-        for line in f:
-            if re.search('^(\d+)/(\d+)/(\d+) ', line):
-                title = line.strip()
-                local[title] = {"text": ""}
-            elif line.strip() != "--" and title in local:
-                local[title]["text"] = local[title]["text"] + line
-
-    # calculate checksum and fix empty lines between entries
-    for title in local.keys():
-        new_text = re.sub("\n\s*\n$", "\n", local[title]["text"])
-        local[title]["text"] = new_text
-        local[title]["checksum"] = md5(new_text)
-
-    return local
-
-
-def read_remote(keep, logger):
-    remote = {}
-    label = keep.findLabel('log')
-
-    note: gkeepapi.node.TopLevelNode
-    for note in keep.find(labels=[label]):
-        if not re.search('^\d+/\d+/\d+ ', note.title):
-            logger.warning(f"{note.title} - Skipping, title mismatch")
-            continue
-        remote[note.title] = {
-            "checksum": md5(note.text),
-            "note": note
-        }
-
-    return remote
-
-
-def read_token(token_file):
-    token = None
-    if exists(token_file):
-        with open(token_file) as f:
-            token = f.read().strip()
-    return token
-
-
-def read_state(state_file):
-    state = None
-    if exists(state_file):
-        with open(state_file) as f:
-            state = json.load(f)
-    return state
-
-
-def read_sync(sync_file):
-    sync = {}
-    if exists(sync_file):
-        with open(sync_file, encoding='utf-8') as f:
-            data = json.load(f)
-            if not "notes" in data:
-                raise ValueError(f"Invalid sync file format in file {sync_file}")
-            notes = data["notes"]
-            for title in notes.keys():
-                sync[title] = notes[title]
-    return sync
-
 
 def md5(s):
     return hashlib.md5(s.encode("utf-8")).hexdigest()
 
 
-def backup_local(backup_dir, file):
-    os.makedirs(backup_dir, exist_ok=True)
-    backup_file = join(backup_dir, datetime.now().strftime("%y%m%d%H%M%S"))
-    shutil.copyfile(file, backup_file)
-
-
 def setup_logger():
     logger = logging.getLogger('keeplog')
     logger.setLevel(logging.INFO)
-    ch = logging.StreamHandler(sys.stdout)
-    formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(message)s')
-    ch.setFormatter(formatter)
-    logger.addHandler(ch)
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setFormatter(logging.Formatter('%(asctime)s [%(levelname)s] %(message)s'))
+    logger.addHandler(handler)
     return logger
+
 
 def load_config():
     return Config().load(expanduser("~/.keeplog/config"))
+
 
 class Keeplog:
     def __init__(self, logger, config):
@@ -244,7 +46,215 @@ class Keeplog:
         self.keep = gkeepapi.Keep()
 
     def sync(self):
-        pass
+        self.login()
+        self.write_token()
+
+        local = self.read_local()
+        remote = self.read_remote()
+        sync = self.read_sync()
+
+        # synchronizing
+        self.logger.info("Comparing remote and local")
+        local_updated = False
+        label = self.keep.getLabel('log')
+
+        for title in local.keys():
+            if not title in remote:
+                self.logger.info(f"- Creating remotely: {title}")
+                note = self.keep.createNote(title, local[title]["text"])
+                note.labels.add(label)
+                sync[title] = {"checksum": md5(local[title]["text"])}
+            elif remote[title]["note"].text != local[title]["text"]:
+                if title in sync:
+                    local_changed = local[title]["checksum"] != sync[title]["checksum"]
+                    remote_changed = remote[title]["checksum"] != sync[title]["checksum"]
+                    if local_changed and not remote_changed:
+                        self.logger.info(f"- Updating remotely: {title}")
+                        remote[title]["note"].text = local[title]["text"]
+                        sync[title] = {"checksum": md5(local[title]["text"])}
+                    elif not local_changed and remote_changed:
+                        self.logger.info(f"- Updating locally: {title}")
+                        local[title]["text"] = remote[title]["note"].text
+                        sync[title] = {"checksum": md5(remote[title]["note"].text)}
+                        local_updated = True
+                    elif self.config.on_conflict == "prefer-remote":
+                        self.logger.info(f"- Updating locally (conflict override): {title}")
+                        local[title]["text"] = remote[title]["note"].text
+                        sync[title] = {"checksum": md5(remote[title]["note"].text)}
+                        local_updated = True
+                    elif self.config.on_conflict == "prefer-local":
+                        self.logger.info(f"- Updating remotely (conflict override): {title}")
+                        remote[title]["note"].text = local[title]["text"]
+                        sync[title] = {"checksum": md5(local[title]["text"])}
+                    else:
+                        self.logger.info(f"- Conflict, doing nothing: {title}")
+                elif self.config.on_conflict == "prefer-remote":
+                    self.logger.info(f"- Updating locally (conflict override): {title}")
+                    local[title]["text"] = remote[title]["note"].text
+                    sync[title] = {"checksum": md5(remote[title]["note"].text)}
+                    local_updated = True
+                elif self.config.on_conflict == "prefer-local":
+                    self.logger.info(f"- Updating remotely (conflict override): {title}")
+                    remote[title]["note"].text = local[title]["text"]
+                    sync[title] = {"checksum": md5(local[title]["text"])}
+                else:
+                    self.logger.info(f"- Conflict, doing nothing: {title}")
+            else:
+                sync[title] = {"checksum": md5(local[title]["text"])}
+
+        self.keep.sync()
+        self.write_state()
+
+        if local_updated:
+            self.backup_local()
+            self.write_local(local)
+        else:
+            self.logger.info("Nothing to update locally")
+
+        self.write_sync(sync)
+
+    def read_local(self):
+        self.logger.info("Reading local notes")
+        local = {}
+
+        # parse file
+        with open(self.config.file, encoding='utf-8') as f:
+            for line in f:
+                if re.search('^(\d+)/(\d+)/(\d+) ', line):
+                    title = line.strip()
+                    local[title] = {"text": ""}
+                elif line.strip() != "--" and title in local:
+                    local[title]["text"] = local[title]["text"] + line
+
+        # calculate checksum and fix empty lines between entries
+        for title in local.keys():
+            new_text = re.sub("\n\s*\n$", "\n", local[title]["text"])
+            local[title]["text"] = new_text
+            local[title]["checksum"] = md5(new_text)
+
+        return local
+
+    def read_remote(self):
+        self.logger.info("Reading remote notes")
+
+        remote = {}
+        label = self.keep.findLabel('log')
+
+        note: gkeepapi.node.TopLevelNode
+        for note in self.keep.find(labels=[label]):
+            if not re.search('^\d+/\d+/\d+ ', note.title):
+                self.logger.warning(f"{note.title} - Skipping, title mismatch")
+                continue
+            remote[note.title] = {
+                "checksum": md5(note.text),
+                "note": note
+            }
+
+        return remote
+
+    def read_token(self):
+        token = None
+        if exists(self.config.token_file):
+            with open(self.config.token_file) as f:
+                token = f.read().strip()
+        return token
+
+    def read_state(self):
+        state = None
+        if exists(self.config.state_file):
+            with open(self.config.state_file) as f:
+                state = json.load(f)
+        return state
+
+    def read_sync(self):
+        sync = {}
+        if exists(self.config.sync_file):
+            with open(self.config.sync_file, encoding='utf-8') as f:
+                data = json.load(f)
+                if not "notes" in data:
+                    raise ValueError(f"Invalid sync file format in file {self.config.sync_file}")
+                notes = data["notes"]
+                for title in notes.keys():
+                    sync[title] = notes[title]
+        return sync
+
+    def backup_local(self):
+        os.makedirs(self.config.backup_dir, exist_ok=True)
+        backup_file = join(self.config.backup_dir, datetime.now().strftime("%y%m%d%H%M%S"))
+        shutil.copyfile(self.config.file, backup_file)
+
+    def login(self):
+        logged_in = False
+
+        token = self.read_token()
+        state = self.read_state()
+
+        if token:
+            logged_in = self.login_with_token(token, state)
+
+        if not logged_in:
+            logged_in = self.login_with_password(state)
+
+        if not logged_in:
+            raise Exception("Failed to authenticate")
+
+    def login_with_token(self, token, state):
+        self.logger.info('Logging in with token')
+        logged_in = False
+
+        try:
+            self.keep.resume(self.config.username, token, state=state, sync=True)
+            self.logger.info('Successfully logged in')
+            logged_in = True
+        except gkeepapi.exception.LoginException:
+            self.logger.warning('Invalid token, falling back to password')
+
+        return logged_in
+
+    def login_with_password(self, state):
+        self.logger.info('Logging in with password')
+        logged_in = False
+
+        try:
+            self.keep.login(self.config.username, self.config.password, state=state, sync=True)
+            self.logger.info('Successfully logged in')
+            logged_in = True
+        except gkeepapi.exception.LoginException:
+            self.logger.info('Login failed')
+
+        return logged_in
+
+    def write_token(self):
+        self.make_parent(self.config.token_file)
+        token = self.keep.getMasterToken()
+        with open(self.config.token_file, 'w') as f:
+            f.write(token)
+
+    def write_state(self):
+        self.make_parent(self.config.state_file)
+        state = self.keep.dump()
+        with open(self.config.state_file, 'w') as f:
+            json.dump(state, f)
+
+    def write_sync(self, sync):
+        self.make_parent(self.config.sync_file)
+        with open(self.config.sync_file, mode="w", encoding="utf-8") as f:
+            data = {"notes": sync}
+            json.dump(data, f)
+
+    def write_local(self, local):
+        self.logger.info("Updating local notes")
+        with open(self.config.file, mode="w", encoding="utf-8") as f:
+            for title in local.keys():
+                f.write(title + "\n")
+                f.write("--\n")
+                f.write(local[title]["text"] + "\n")
+                if not local[title]["text"].endswith("\n"):
+                    f.write("\n")  # ensure empty line between entries
+
+    def make_parent(self, file):
+        Path(file).parent.mkdir(mode=0o755, parents=True, exist_ok=True)
+
 
 class Config:
     def __init__(self):
